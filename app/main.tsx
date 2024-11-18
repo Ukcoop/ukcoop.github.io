f7bd795ea82349b1dad8cca870cbbf487526f33a
Alexander Cooper <coop.main@protonmail.com>
2024-11-18 16:23:59 -0700

    should compile now

---
+++ app/main.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useState } from 'react';
+
+import Navbar from './components/navbar';
+
+import Home from './pageComponents/Home';
+import About from './pageComponents/About';
+
+export default function Main() {
+  const [index, setIndex] = useState(0);
+
+  const pages = [
+    <Home key="0"/>,
+    <About key="1"/>
+  ]
+
+  return (
+    <div className="flex flex-row h-svh max-h-svh overflow-crop">
+      <Navbar setIndex={setIndex}/>
+      {pages[index]}
+    </div> 
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
 'use client';
 
-import { useState } from 'react';
+import dynamic from 'next/dynamic';
 
-import Navbar from './components/navbar';
+const Main = dynamic(() => import('./main'), { ssr: false });
 
-import Home from './pageComponents/Home';
-import About from './pageComponents/About';
-
-export default function Main() {
-  const [index, setIndex] = useState(0);
-
-  const pages = [
-    <Home key="0"/>,
-    <About key="1"/>
-  ]
-
-  return (
-    <div className="flex flex-row h-svh max-h-svh overflow-crop">
-      <Navbar setIndex={setIndex}/>
-      {pages[index]}
-    </div> 
-  );
+export default function Page() {
+  return <Main />;
 }
