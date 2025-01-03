757885e8ae4c8153a1d284206be11d1b25991d1b
Alexander Cooper <coop.main@protonmail.com>
2025-01-03 12:56:09 -0700

    forgot i needed to lint

--- app/main.tsx
+++ app/main.tsx
@@ -1,24 +1,11 @@
 'use client';
 
-import { useState } from 'react';
-
-import Navbar from './components/navbar';
-
 import Home from './pageComponents/Home';
-import About from './pageComponents/About';
 
 export default function Main() {
-  const [index, setIndex] = useState(0);
-
-  const pages = [
-    <Home key="0"/>,
-//    <About key="1"/>
-  ]
-
   return (
     <div className="flex flex-row h-svh max-h-svh overflow-crop">
-      {/*<Navbar currentIndex={index} setIndex={setIndex}/>*/}
-      {pages[0]}
+      {<Home key="0"/>}
     </div> 
   );
 }
